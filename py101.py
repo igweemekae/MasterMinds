77331667993de3b69159c1f6b277782318aae62b
EzeObiIgwe <igweemekae@gmail.com>
2021-05-31 19:29:17 +0100

    text works2

---
+++ py101.py
@@ -0,0 +1,7 @@
+number1 = int(input("What is the first no?"))
+number2 = int(input("What is the second no?"))
+number3= int(input("What is the third no?"))
+
+largest_num = max(number1, number2, number3)
+
+print("the largest of the 3 values is ", largest_num)
